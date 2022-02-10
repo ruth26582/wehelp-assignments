e7eae252e9c445655b7e6d987ed41adf8e59fea3
RubyLo <ruth265820@gmaill.com>
2022-02-10 17:38:55 +0800

    基礎會員系統開發
    整合前端、後端，完成簡易會員註冊、登入、登出系統。

---
+++ week-6/main.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
+
+app = Flask(__name__)  # __name__代表目前執行的模組
+app.secret_key = "#084$*2^n-_q+!qsf1d=zu967$dqs_xd0typdy!0&4+b(db7!f^f"
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="12345678",
+    database="website"
+)
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = db.cursor()
+        cursor.execute("select username from member")
+        result = cursor.fetchall()
+        for i in range(len(result)):
+            print(result[i][0])
+            if result[i][0] == username:
+                cursor.close()
+                return redirect(url_for("error", message='帳號已經被註冊'))
+        else:
+            if name == '' or username == '' or password == '':
+                return redirect(url_for("error", message='請輸入姓名、帳號、密碼'))
+            else:
+                cursor.execute(
+                    "INSERT INTO member VALUES (NULL,%s,%s,%s,DEFAULT,DEFAULT);", (name, username, password))
+                cursor.close()
+                db.commit()
+                return redirect(url_for("login"))
+
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = db.cursor()
+        cursor.execute("select username, password from member;")
+        result = cursor.fetchall()
+        for i in range(len(result)):
+            if (username == result[i][0]) and (password == result[i][1]):
+                session['username'] = username
+                cursor.close()
+                return redirect(url_for("member"))
+
+        if (username == '' and password == '') or (username == '' and password != '') or (username != '' and password == ''):
+            return redirect(url_for("error", message='請輸入帳號、密碼'))
+        else:
+            return redirect(url_for("error", message='帳號、或密碼輸入錯誤'))
+
+    else:
+        if "username" in session:
+            return redirect(url_for("member"))
+        return render_template("login.html")
+
+
+@app.route("/member/")
+def member():
+    if "username" in session:
+        username = session["username"]
+        cursor = db.cursor()
+        cursor.execute("select name, username from member;")
+        results = cursor.fetchall()
+        for x in results:
+            if username in x:
+                name = x[0]
+        return render_template("member.html", accountVal=username, name=name)
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/error/")
+def error():
+    errMsg = request.args.get('message')
+    return render_template("error.html", message=errMsg)
+
+
+@ app.route("/signout")
+def signout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # 立刻啟動伺服器
